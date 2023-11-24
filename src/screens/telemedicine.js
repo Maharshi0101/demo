60261b6ce32406e804a260b29d6f71c7669ab02d
Maharshi0101 <maharshi.ganji01@gmail.com>
2023-11-24 17:09:41 +0530

    added axios library

--- src/screens/telemedicine.js
+++ src/screens/telemedicine.js
@@ -2,11 +2,41 @@ import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
 import { useTheme } from '../contexts/theme';
 import { useLanguage } from '../contexts/language'
+import axios from 'axios';
 
 export default function Telemedicine({ navigation, route }) {
+
     const { currentTheme } = useTheme();
     const { strings } = useLanguage()
     const specialityList = (route && route.params && route.params.details) || undefined
+
+    useEffect(() => {
+        getSpecialistsList()
+    }, [])
+
+    async function getSpecialistsList() {
+        try {
+
+            // Set your API endpoint
+            const apiUrl = 'https://test-api.bupa.com.sa/bupa-organization/point/careconnectapi/speciality';
+
+            // Define your headers
+            const headers = {
+                'Content-Type': 'application/json',
+                // 'Authorization': 'Bearer <Bearer Token>',
+                'X-IBM-Client-Id': 'ff2c78972b6d1ee0efb2e07a128ef1fe',
+                'X-IBM-Client-Secret': '9f38c7a03dfaf209b41bedb011e15064'
+            };
+
+            // Make the GET request using Axios
+            const response = await axios.get(apiUrl, { headers });
+
+            console.log(JSON.stringify(response));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
 
     return (
         <View style={{ flex: 1, marginBottom: 20 }}>
